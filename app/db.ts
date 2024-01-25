080ef5adb915d4bc18f8eb88eff513917525c48f
patrick <p.ch@live.co.uk>
2024-01-25 17:24:15 +0800

    make table

--- app/db.ts
+++ app/db.ts
@@ -10,19 +10,39 @@ import { genSaltSync, hashSync } from 'bcrypt-ts';
 let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
 let db = drizzle(client);
 
-let users = pgTable('User', {
+let usersTable = pgTable('users', {
   id: serial('id').primaryKey(),
-  email: varchar('email', { length: 64 }),
-  password: varchar('password', { length: 64 }),
+  email: varchar('email', { length: 255 }),
+  password: varchar('password', { length: 255 }),
 });
 
 export async function getUser(email: string) {
-  return await db.select().from(users).where(eq(users.email, email));
+  return await db.select().from(usersTable).where(eq(usersTable.email, email));
 }
 
 export async function createUser(email: string, password: string) {
   let salt = genSaltSync(10);
   let hash = hashSync(password, salt);
 
-  return await db.insert(users).values({ email, password: hash });
+  return await db.insert(usersTable).values({ email, password: hash });
 }
+
+/*
+import { sql } from '@vercel/postgres'
+import { InferSelectModel } from 'drizzle-orm'
+
+export type User = InferSelectModel<typeof usersTable>;
+
+async function seed() {
+  // Create table with raw SQL
+  const newTable = await sql.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255),
+        "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+      );
+  `);
+  console.log(`Created "users" table`);
+}
+*/
