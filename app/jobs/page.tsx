00b094efdeb0d758091e94f7c1ee744d7138ed03
patrick <p.ch@live.co.uk>
2024-01-26 12:20:27 +0800

    make jobs list page very slightly nicer

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -10,15 +10,28 @@ import houseIcon from "@/app/assets/house.png"
 
 function JobItem(job: any) {
   return (
-    <Link href={`/job/${job.id}`}>
-      <li key={job.id} className="flex p-5 hover:bg-gray-200 items-center overflow-hidden">
-        <Image className="h-8 w-auto m-4" src={houseIcon} alt="building" />
-        <div className="text-sm font-semibold leading-6 text-gray-900">
-          <p>Job ID: {job.id} User {job.userId}</p>
-          <p>{JSON.stringify(job.info)}</p>
+    <li key={job.id}>
+      <Link href={`/job/${job.id}`} className="flex justify-between gap-x-6 p-8 hover:bg-gray-300">
+
+        <div className="flex min-w-0 gap-x-4">
+          <Image className="h-12 w-12 flex-none rounded-full" src={houseIcon} alt="building" />
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">Job ID: {job.id} - Kenya - 2 Story building</p>
+            <p className="mt-1 truncate text-xs leading-5 text-gray-500">Owner ID: {job.userId} example@haru.com</p>
+          </div>
         </div>
-      </li>
-    </Link>
+
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p className="text-sm leading-6 text-gray-900">
+            Status: pending
+          </p>
+          <p className="mt-1 text-xs leading-5 text-gray-500">
+            Last updated <time dateTime="2023-01-23T13:23Z">3h ago</time>
+          </p>
+        </div>
+
+      </Link>
+    </li>
   )
 }
 
@@ -41,12 +54,6 @@ export default async function Page() {
           List of Available Jobs
         </h1>
 
-        {/* <ul className="list-disc list-inside">
-          <li>TODO Should only be visible to contractors</li>
-          <li>Maybe allow the possibility for clients to allow it to be public</li>
-        </ul>
-      
-       */}
         <Suspense fallback={<p>Loading ...</p>}>
           <JobList />
         </Suspense>
