e5bea29fe10d702460a127886a0995da8920aa5e
patrick <p.ch@live.co.uk>
2024-02-19 21:31:52 +0800

    side by side questionairre

--- app/job-posting/page.tsx
+++ app/job-posting/page.tsx
@@ -1,10 +1,9 @@
 import { PhotoIcon, } from '@heroicons/react/24/solid';
 import SimpleLayout from '@/app/components/layout';
-import { submitJobPost } from '@/app/actions';
+import { submitJobPost } from '@/lib/actions';
 
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { cn } from '@/lib/utils';
 import { Separator } from '@/components/ui/separator';
 import { Textarea } from '@/components/ui/textarea';
 
@@ -73,53 +72,86 @@ function HouseTypeSelection() {
   };
 
   return (
-    <RadioGroup defaultValue="single-family-home" className="grid gap-4 grid-cols-4 grid-rows-1 py-8">
-      <div>
-        <h2 className="py-4">Residential</h2>
+    <>
+      <RadioGroup defaultValue="single-family-home" className="grid gap-4 grid-cols-4 grid-rows-1 py-8">
         <div>
-          {residentialTypes.map((n, i) => <RadioOption key={i} id={i} name={n} />)}
+          <h2 className="py-4">Residential</h2>
+          <div>
+            {residentialTypes.map((n, i) => <RadioOption key={i} id={i} name={n} />)}
+          </div>
         </div>
-      </div>
-      <div>
-        <h2 className="py-4">Commercial</h2>
-        {commercialTypes.map((n, i) => <RadioOption key={i} id={i+100} name={n} />)}
-      </div>
-      <div>
-        <h2 className="py-4">Entertainment</h2>
-        {entertainmentTypes.map((n, i) => <RadioOption key={i} id={i+200} name={n} />)}
-      </div>
-      <div>
-        <h2 className="py-4">Industrial</h2>
-        {industrialTypes.map((n, i) => <RadioOption key={i} id={i+300} name={n} />)}
-      </div>
-    </RadioGroup>
+        <div>
+          <h2 className="py-4">Commercial</h2>
+          {commercialTypes.map((n, i) => <RadioOption key={i} id={i + 100} name={n} />)}
+        </div>
+        <div>
+          <h2 className="py-4">Entertainment</h2>
+          {entertainmentTypes.map((n, i) => <RadioOption key={i} id={i + 200} name={n} />)}
+        </div>
+        <div>
+          <h2 className="py-4">Industrial</h2>
+          {industrialTypes.map((n, i) => <RadioOption key={i} id={i + 300} name={n} />)}
+        </div>
+      </RadioGroup>
+    </>
   )
 }
 
 
 
-const questions = [
-  { name: "lifestyle", title: "Lifestyle", hints: [
-    "How many people will be living in the house?",
-    "Are there any other specific lifestyle preferences or routines to consider?"
-  ]},
-  { name: "future", title: "Future Plans", hints: [
-    "Are there any plans for future expansions or modifications to the house?",
-    "Do you have any long-term considerations, such as aging in place?"
-  ]},
-  { name: "energy", title: "Energy Efficiency and Sustainability", hints: [
-    "Are you interested in incorporating energy-efficient or sustainable design elements?",
-    "Do you have any preferences for eco-friendly materials?"
-  ]},
-];
-
 function Questions() {
+
+
+  const questions = [
+    {
+      name: "lifestyle", title: "Lifestyle", hints: [
+        "How many people will be living in the house?",
+        "Are there any other specific lifestyle preferences or routines to consider?"
+      ]
+    },
+    {
+      name: "future", title: "Future Plans", hints: [
+        "Are there any plans for future expansions or modifications to the house?",
+        "Do you have any long-term considerations, such as aging in place?"
+      ]
+    },
+    {
+      name: "energy", title: "Energy Efficiency and Sustainability", hints: [
+        "Are you interested in incorporating energy-efficient or sustainable design elements?",
+        "Do you have any preferences for eco-friendly materials?"
+      ]
+    },
+    {
+      name: "outdoors", title: "Outdoor Spaces", hints: [
+        "Do you have any preferences for outdoor spaces such as gardens, patios, or decks?",
+        "Are there specific views or orientations you would like to take advantage of?"
+      ]
+    },
+    {
+      name: "security", title: "Privacy and Security", hints: [
+        "How important is privacy to you?",
+        "Are there specific security considerations we should address?"
+      ]
+    },
+    {
+      name: "maintenance", title: "Maintenance Preference", hints: [
+        "Are there specific materials or finishes you prefer for ease of maintenance?",
+        "What level of maintenance are you comfortable with for the long term?"
+      ]
+    },
+    {
+      name: "special", title: "Special Requirements", hints: [
+        "Do you have any specific needs such as accessibility features or special accommodations?",
+      ]
+    },
+  ];
+
   return (
     <>
       {questions.map(
         (qa, i) => (
           <div key={i} className="flex flex-col">
-            <div className="flex flex">
+            <div className="flex space-x-8">
               <div className="w-1/2 flex flex-col space-y-2">
                 <label htmlFor={qa.name} className="text-sm font-medium text-gray-900">
                   {qa.title}
@@ -140,132 +172,35 @@ function Questions() {
           </div>
         )
       )}
+    </>
+  );
+}
 
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="technology" className="block text-sm font-medium text-gray-900">
-          Technology Integratioy
-        </label>
-        <ul className="list-disc list-inside text-sm text-gray-600">
-          <li key={0}>Are there specific technology features you would like to integrate into your home?</li>
-          <li key={1}>Smart home systems, automation, etc.</li>
-        </ul>
-        <textarea
-          name="technology"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm"
-          defaultValue={''}
-        />
-      </div>
+function ExtraFiles() {
+  return (
+    <div>
+      <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+        Do you have any files to attach?
+      </label>
+      <p className="block text-sm text-gray-900">Archutectural docments, land survey documents, legal documents</p>
 
-      <Separator className="my-4" />
-
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="outdoors" className="block text-sm font-medium text-gray-900">
-          Outdoor Spaces
-        </label>
-        <ul className="list-disc list-inside text-sm text-gray-600">
-          <li key={0}>Do you have any preferences for outdoor spaces such as gardens, patios, or decks?</li>
-          <li key={1}>Are there specific views or orientations you would like to take advantage of?</li>
-        </ul>
-        <textarea
-          name="outdoors"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm"
-          defaultValue={''}
-        />
-      </div>
-
-      <Separator className="my-4" />
-
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="security" className="block text-sm font-medium text-gray-900">
-          Privacy and Security
-        </label>
-        <ul className="list-disc list-inside text-sm text-gray-600">
-          <li key={0}>How important is privacy to you?</li>
-          <li key={1}>Aretherespecificsecurityconsiderationsweshouldaddress?</li>
-        </ul>
-        <textarea
-          name="security"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm"
-          defaultValue={''}
-        />
-      </div>
-
-      <Separator className="my-4" />
-
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="security" className="block text-sm font-medium text-gray-900">
-          Regulatory and Zoning Requirement
-        </label>
-        <ul className="list-disc list-inside text-sm text-gray-600">
-          <li key={0}>Are there any local zoning or regulatory requirements we need to consider in the design?</li>
-          <li key={1}>Have you already obtained necessary permits for the construction?</li>
-        </ul>
-        <textarea
-          name="security"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm"
-          defaultValue={''}
-        />
-      </div>
-
-      <Separator className="my-4" />
-
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="maintenance" className="block text-sm font-medium text-gray-900">
-          Maintenance Preference
-        </label>
-        <ul className="list-disc list-inside text-sm text-gray-600">
-          <li key={0}>Are there specific materials or finishes you prefer for ease of maintenance?</li>
-          <li key={1}>What level of maintenance are you comfortable with for the long term?</li>
-        </ul>
-        <textarea
-          name="maintenance"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm"
-          defaultValue={''}
-        />
-      </div>
-
-      <Separator className="my-4" />
-
-      <div className="flex flex-col space-y-2">
-        <label htmlFor="special" className="block text-sm font-medium text-gray-900">
-          Special Requirements
-        </label>
-        <ul className="list-disc list-inside text-sm text-gray-600">
-          <li key={0}>Do you have any specific needs such as accessibility features or special accommodations?</li>
-        </ul>
-        <textarea
-          name="special"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm"
-          defaultValue={''}
-        />
-      </div>
-
-      <Separator className="my-4" />
-
-      <div>
-        <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-          Do you have any files to attach?
-        </label>
-        <p className="block text-sm text-gray-900">Archutectural docments, land survey documents, legal documents</p>
-
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-          <div className="text-center">
-            <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-            <div className="mt-4 flex text-sm text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-              >
-                <span>Upload a file</span>
-                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-              </label>
-              <p className="pl-1">or drag and drop</p>
-            </div>
-            <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+        <div className="text-center">
+          <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+          <div className="mt-4 flex text-sm text-gray-600">
+            <label
+              htmlFor="files"
+              className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+            >
+              <span>Upload a file</span>
+              <input id="files" name="files" type="file" className="sr-only" />
+            </label>
+            <p className="pl-1">or drag and drop</p>
           </div>
+          <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
@@ -278,6 +213,10 @@ function Form() {
       <Separator className="my-4" />
 
       <Questions />
+
+      <ExtraFiles />
+
+      <Separator className="my-4" />
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
