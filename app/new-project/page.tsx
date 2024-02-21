da3b3c7ae82e6f557ece4c2acccf06f6b28fa23d
patrick <p.ch@live.co.uk>
2024-02-22 05:12:43 +0800

    move questions and building type selector to separate file

--- app/new-project/page.tsx
+++ app/new-project/page.tsx
@@ -3,157 +3,17 @@ import SimpleLayout from '@/components/layout';
 import { submitProjectPost } from '@/lib/actions';
 
 import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Separator } from '@/components/ui/separator';
 import { Textarea } from '@/components/ui/textarea';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import CountrySelector from './country-selector-simple';
-// import CountrySelector from './country-selector';
+import HouseTypeSelection from './building-type-selector';
 
-function HouseTypeSelection() {
-
-  const residentialTypes = [
-    "Single Family Home",
-    "Multi Family Home",
-    "Town House",
-    "Apartment",
-    "Condominuim",
-    "Mobile Home",
-    "Modular Home",
-    "Student Housing",
-    "Duplexe",
-    "Mansion",
-    "Others (specify)",
-  ];
-
-  const commercialTypes = [
-    "Office Building",
-    "Convention Center",
-    "Shopping Center/Mall",
-    "Hotel",
-    "Restaurant",
-    "Bank",
-    "Medical Facility",
-    "Educational Building",
-    "Retail Building",
-    "Parking Structure",
-    "Others (specify)",
-  ];
-
-  const entertainmentTypes = [
-    "Theater",
-    "Museum/Art Gallery",
-    "Sports Facility",
-    "Amphitheater",
-    "Cinema",
-    "Night Club",
-    "Casino",
-    "Theme Park",
-    "Concert Hall",
-    "Library",
-    "Others (specify)",
-  ];
-
-  const industrialTypes = [
-    "Factory",
-    "Mills",
-    "Processing Plant",
-    "Cold Storage Facility",
-    "Data Center",
-    "Warehouse",
-    "Others (specify)",
-  ];
-
-  function RadioOption({ name, id }: { name: string, id: number }) {
-    const value1 = name.toLowerCase().replace(' ', '-');
-    return (
-      <Label className="flex items-center space-x-2 p-2 hover:bg-gray-100" htmlFor={`r${id}`} >
-        <RadioGroupItem value={value1} id={`r${id}`} />
-        <span>{name}</span>
-      </Label>
-    )
-  };
-
-  return (
-    <div>
-      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-        Building Type
-      </h4>
-
-      <RadioGroup name="type" defaultValue="single-family-home" className="grid gap-4 grid-cols-4 grid-rows-1">
-        <div>
-          <h4 className="py-4">Residential</h4>
-          <div>
-            {residentialTypes.map((n, i) => <RadioOption key={i} id={i} name={n} />)}
-          </div>
-        </div>
-        <div>
-          <h4 className="py-4">Commercial</h4>
-          {commercialTypes.map((n, i) => <RadioOption key={i} id={i + 100} name={n} />)}
-        </div>
-        <div>
-          <h4 className="py-4">Entertainment</h4>
-          {entertainmentTypes.map((n, i) => <RadioOption key={i} id={i + 200} name={n} />)}
-        </div>
-        <div>
-          <h4 className="py-4">Industrial</h4>
-          {industrialTypes.map((n, i) => <RadioOption key={i} id={i + 300} name={n} />)}
-        </div>
-      </RadioGroup>
-    </div>
-  )
-}
-
-
+import { questions } from 'content/questions';
 
 function Questions() {
-
-  const questions = [
-    {
-      name: "lifestyle", title: "Lifestyle", hints: [
-        "How many people will be living in the house?",
-        "Are there any other specific lifestyle preferences or routines to consider?"
-      ]
-    },
-    {
-      name: "future", title: "Future Plans", hints: [
-        "Are there any plans for future expansions or modifications to the house?",
-        "Do you have any long-term considerations, such as aging in place?"
-      ]
-    },
-    {
-      name: "energy", title: "Energy Efficiency and Sustainability", hints: [
-        "Are you interested in incorporating energy-efficient or sustainable design elements?",
-        "Do you have any preferences for eco-friendly materials?"
-      ]
-    },
-    {
-      name: "outdoors", title: "Outdoor Spaces", hints: [
-        "Do you have any preferences for outdoor spaces such as gardens, patios, or decks?",
-        "Are there specific views or orientations you would like to take advantage of?"
-      ]
-    },
-    {
-      name: "security", title: "Privacy and Security", hints: [
-        "How important is privacy to you?",
-        "Are there specific security considerations we should address?"
-      ]
-    },
-    {
-      name: "maintenance", title: "Maintenance Preference", hints: [
-        "Are there specific materials or finishes you prefer for ease of maintenance?",
-        "What level of maintenance are you comfortable with for the long term?"
-      ]
-    },
-    {
-      name: "special", title: "Special Requirements", hints: [
-        "Do you have any specific needs such as accessibility features or special accommodations?",
-      ]
-    },
-  ];
-
   return (
     <>
       {questions.map(
---
+++ content/questions.ts
@@ -0,0 +1,45 @@
+
+
+export const questions = [
+    {
+        name: "lifestyle", title: "Lifestyle", hints: [
+            "How many people will be living in the house?",
+            "Are there any other specific lifestyle preferences or routines to consider?"
+        ]
+    },
+    {
+        name: "future", title: "Future Plans", hints: [
+            "Are there any plans for future expansions or modifications to the house?",
+            "Do you have any long-term considerations, such as aging in place?"
+        ]
+    },
+    {
+        name: "energy", title: "Energy Efficiency and Sustainability", hints: [
+            "Are you interested in incorporating energy-efficient or sustainable design elements?",
+            "Do you have any preferences for eco-friendly materials?"
+        ]
+    },
+    {
+        name: "outdoors", title: "Outdoor Spaces", hints: [
+            "Do you have any preferences for outdoor spaces such as gardens, patios, or decks?",
+            "Are there specific views or orientations you would like to take advantage of?"
+        ]
+    },
+    {
+        name: "security", title: "Privacy and Security", hints: [
+            "How important is privacy to you?",
+            "Are there specific security considerations we should address?"
+        ]
+    },
+    {
+        name: "maintenance", title: "Maintenance Preference", hints: [
+            "Are there specific materials or finishes you prefer for ease of maintenance?",
+            "What level of maintenance are you comfortable with for the long term?"
+        ]
+    },
+    {
+        name: "special", title: "Special Requirements", hints: [
+            "Do you have any specific needs such as accessibility features or special accommodations?",
+        ]
+    },
+];
