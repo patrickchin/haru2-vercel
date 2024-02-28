a8553ed5dd90e35c07cc185720d1468055745ee7
patrick <p.ch@live.co.uk>
2024-02-29 05:44:41 +0800

    move building types to a separate file

---
+++ content/buildingTypes.ts
@@ -0,0 +1,68 @@
+
+export const buildingTypes: { type: string, subtypes: string[] }[] = [
+
+  {
+    type: "Residential",
+    subtypes: [
+      "Single Family Home",
+      "Multi Family Home",
+      "Town House",
+      "Apartment",
+      "Condominuim",
+      "Mobile Home",
+      "Modular Home",
+      "Student Housing",
+      "Duplexe",
+      "Mansion",
+      "Others (specify)",
+    ]
+  },
+
+  {
+    type: "Commercial",
+    subtypes: [
+      "Office Building",
+      "Convention Center",
+      "Shopping Center/Mall",
+      "Hotel",
+      "Restaurant",
+      "Bank",
+      "Medical Facility",
+      "Educational Building",
+      "Retail Building",
+      "Parking Structure",
+      "Others (specify)",
+    ]
+  },
+
+  {
+    type: "Entertainment",
+    subtypes: [
+      "Theater",
+      "Museum/Art Gallery",
+      "Sports Facility",
+      "Amphitheater",
+      "Cinema",
+      "Night Club",
+      "Casino",
+      "Theme Park",
+      "Concert Hall",
+      "Library",
+      "Others (specify)",
+    ]
+  },
+
+  {
+    type: "Industrial",
+    subtypes: [
+      "Factory",
+      "Mills",
+      "Processing Plant",
+      "Cold Storage Facility",
+      "Data Center",
+      "Warehouse",
+      "Others (specify)",
+    ]
+  },
+
+]
