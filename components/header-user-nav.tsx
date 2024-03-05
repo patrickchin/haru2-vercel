7b573a29014c47cae86fc68ab9b400a7a4cd9725
patrick <p.ch@live.co.uk>
2024-03-05 17:36:29 +0800

    remove old new-project form

--- components/header-user-nav.tsx
+++ components/header-user-nav.tsx
@@ -6,8 +6,7 @@ import { useSession } from "next-auth/react"
 import { Button } from '@/components/ui/button';
 
 const navigation = [
-  { name: 'New Project', href: '/new-project', },
-  { name: 'New Project 2', href: '/new-project2', },
+  { name: 'New Project', href: '/new-project2', },
   { name: 'Projects', href: '/projects', },
   { name: 'About', href: '/about', },
   // { name: 'Calendar', href: '#', },
