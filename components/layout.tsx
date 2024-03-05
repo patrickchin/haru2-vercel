47f463cc9b90dc4a3a06d0542aa1653d987cf1c8
patrick <p.ch@live.co.uk>
2024-03-05 19:18:01 +0800

    playing with bg gradients

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ export default function SimpleLayout({ children } : { children : React.ReactNode
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="grow flex flex-col bg-gradient-to-r from-cyan-100 to-blue-100">
+      <main className="grow flex flex-col bg-gradient-to-r from-cyan-50/80 to-red-50/80">
         <div className="grow flex flex-col w-screen mx-auto max-w-6xl px-12">
           <div className="grow flex flex-col bg-background shadow-xl p-16">
             {children}
