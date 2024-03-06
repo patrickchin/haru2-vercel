34f6b0c50aa4da44f3db6d066b071d9c1dbb4514
patrick <p.ch@live.co.uk>
2024-03-06 16:35:37 +0800

    don't use dotenv ...

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config({ path: ".env.local" });
- 
+
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
