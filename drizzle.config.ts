8e96cfc210a0893e0948122624e58404a2d2b856
patrick <p.ch@live.co.uk>
2024-03-08 06:36:11 +0800

    add dotenv back in drizzle config for the drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,20 @@
 import type { Config } from "drizzle-kit";
 
+// needed for drizzle studio
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   driver: 'pg',
   dbCredentials: {
-    connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
-    // user: process.env.POSTGRES_USER,
-    // password: process.env.POSTGRES_PASSWORD,
-    // host: process.env.POSTGRES_HOST!,
-    // port: 5432,
-    // database: process.env.POSTGRES_DATABASE!,
+    // connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST!,
+    port: 5432,
+    database: process.env.POSTGRES_DATABASE!,
+    ssl: true,
   }
 } satisfies Config;
