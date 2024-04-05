80dc12aefc6abc6ffeacbe12e4d6ff2073f9b983
patrick <p.ch@live.co.uk>
2024-04-06 06:04:04 +0800

    view an obj file and rotate the model!

--- app/project/[id]/components/project-model-view.tsx
+++ app/project/[id]/components/project-model-view.tsx
@@ -1,8 +1,16 @@
 "use client"
 
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stats } from '@react-three/drei';
+
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { useLoader } from '@react-three/fiber'
+
 
 export default function ProjectModelView({ project, }: { project: any }) {
+
+  const obj = useLoader(OBJLoader, '/Bambo_House.obj');
+
   return (
     <div className="border rounded-lg h-[500px]">
       <Canvas
@@ -14,10 +22,16 @@ export default function ProjectModelView({ project, }: { project: any }) {
       >
         <ambientLight color={"white"} intensity={0.3} />
         <directionalLight color="white" position={[0, 2, 5]} />
-        <mesh receiveShadow={true} castShadow  position={[0, 0, 0]}>
+
+        <primitive receiveShadow={true} castShadow object={obj} position={[-5, -2, 6]} />
+
+        <mesh receiveShadow={true} castShadow  position={[-13, 2, 0]}>
           <boxGeometry args={[6, 10, 10]} />
           <meshPhysicalMaterial color="red" />
         </mesh>
+
+        <OrbitControls enableDamping={false} />
+        <Stats />
       </Canvas>
     </div>
   );
