3a20765daf49d0df86ab7f031120bff020583438
patrick <p.ch@live.co.uk>
2024-04-15 22:33:26 +0800

    teams progrewss restructure

--- app/project/[id]/components/project-teams-progress.tsx
+++ app/project/[id]/components/project-teams-progress.tsx
@@ -2,49 +2,65 @@
 
 import * as React from "react"
 import TaskTable from "./task-table"
-import { LucideChevronDown } from "lucide-react"
+import { LucideChevronDown, LucideListCollapse } from "lucide-react"
 import { Progress } from "@/components/ui/progress"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
 
-import { getProjectTasks } from "@/lib/actions"
 import { DesignTask } from '@/lib/types'
 import { DesignProject } from '@/lib/types'
 
-function TeamProgress({ project, team }: any) {
+const teamNames: Record<string, string> = {
+  legal: "Legal",
+  architectural: "Architectural",
+  structural: "Structural",
+  mep: "Mechanical, Electrical and Plumbing",
+  other: "Other",
+};
 
-  const total = team.completed + team.inprogress + team.pending;
-  const pct = 100 * team.completed / total;
-  const col = pct < 30 ? "bg-teal-300" :
-    pct < 80 ? "bg-emerald-300" : "bg-green-300";
+function TeamProgress({ project, team, tasks }: {
+  project: DesignProject,
+  team: string,
+  tasks: DesignTask[],
+}) {
+
+  tasks = tasks.filter((task) => task.type == team);
+
+  const completed = tasks.reduce((n, t) => (t.status === "completed" ? n + 1 : n), 0)
+  const total = tasks.length;
+  const pct = total ? 100 * completed / total : 0;
+  // const col = pct < 30 ? "bg-teal-300" :
+  //   pct < 80 ? "bg-emerald-300" : "bg-green-300";
+  const col = "bg-blue-400"
 
   return (
     <Card>
-      <CardHeader className="flex flex-row justify-between gap-8 p-8">
-        <CardTitle>
-          {team.name}
-        </CardTitle>
-        {/* can't do the colors programatically with tailwind!! */}
-        <Progress value={pct} indicatorColor={col} />
+      <CardHeader className="flex flex-row gap-8 p-8 items-end">
+        <div className="flex w-1/2 gap-4 items-end">
+          <CardTitle>{teamNames[team]}</CardTitle>
+          <span className="text-sm">Team Lead: </span>
+        </div>
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
+        {/* <div className="flex justify-center">
+          <Progress className="w-1/3 h-2" value={pct} indicatorColor={col} />
+        </div> */}
         <Collapsible className="space-y-4">
           <CollapsibleTrigger className="flex justify-between gap-8 w-full p-4 justify-start text-start font-semibold text-sm rounded-md hover:bg-accent">
-            <span>Completed {team.completed} of {total} total tasks ({pct.toFixed(0)} %)</span>
+            <span className="flex gap-4 items-center">
+              <LucideListCollapse />
+              <span>Completed {completed} of {total} total tasks ({pct.toFixed(0)} %)</span>
+            </span>
             <span className="flex flex-row gap-2 items-center">Expand for details <LucideChevronDown/></span>
           </CollapsibleTrigger>
           <CollapsibleContent>
-            <TaskTable projectid={project.id} data={team.tasks} />
+            <TaskTable projectid={project.id} data={tasks} />
           </CollapsibleContent>
         </Collapsible>
 
       </CardContent>
     </Card>);
 }
-
-
-
-
 
 export function ProjectTeamsProgressSkeleton() {
   return (
@@ -56,23 +72,11 @@ export function ProjectTeamsProgressSkeleton() {
 
 export default function ProjectTeamsProgress({ project, tasks }: { project: DesignProject, tasks: DesignTask[] }) {
 
-  const legalTasks = tasks.filter((task) => task.type == "legal");
-  const architecturalTasks = tasks.filter((task) => task.type == "architectural");
-  const structuralTasks = tasks.filter((task) => task.type == "structural");
-  const mepTasks = tasks.filter((task) => task.type == "mep");
-  const otherTasks = tasks.filter((task) => task.type == "other");
-
-  // TODO actually put the above information into this array
-  const progressValues = [
-    { name: "Legal", completed: 20, inprogress: 4, pending: 0, tasks: legalTasks },
-    { name: "Architectural", completed: 23, inprogress: 1, pending: 0, tasks: architecturalTasks },
-    { name: "Structural", completed: 5, inprogress: 3, pending: 20, tasks: structuralTasks },
-    { name: "Mechanical, Electrical and Plumbing", completed: 3, inprogress: 1, pending: 17, tasks: mepTasks },
-  ]
+   const teams = [ "legal", "architectural", "structural", "mep", "other" ];
 
   return (
     <div className="flex flex-col space-y-4">
-      {progressValues.map((team, i) => <TeamProgress key={i} project={project} team={team} />)}
+      {teams.map((team, i) => <TeamProgress key={team} project={project} team={team} tasks={tasks} />)}
     </div>
   );
 }
