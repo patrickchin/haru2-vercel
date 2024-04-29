f5a0f1c50e9b0a3aa2166e66b403e746a170ced4
Binayak Ray <csebinayak.ku@gmail.com>
2024-04-29 17:41:03 +0600

    add deisgn of the ui

--- components/editable-title.tsx
+++ components/editable-title.tsx
@@ -2,40 +2,76 @@
 
 import { DesignProject } from "@/lib/types";
 import { Button } from "./ui/button";
-import { LucidePencil } from "lucide-react";
+import { LucidePencil, CheckSquare, XSquare } from "lucide-react";
 import { useState } from "react";
 import { Input } from "./ui/input";
 
-export default function EditableTitle({ project }: { project: DesignProject }) {
+export default function EditableTitle({
+  project,
+  // handleSubmitProjectForm,
+}: {
+  project: DesignProject;
+  // handleSubmitProjectForm: () => void;
+}) {
   const [title, setTitle] = useState(project.title || "Untitled");
   const [editing, setEditing] = useState(false);
 
-  // if (!editing) {
-  //   return (
-  //     <h3 className="flex items-center gap-1">
-  //       {project.title || "Untitled"}
-  //       <Button
-  //         variant="ghost"
-  //         className="p-2 text-muted-foreground"
-  //         onClick={() => setEditing(true)}
-  //       >
-  //         <LucidePencil className="fg-muted w-4 p-0" />
-  //       </Button>
-  //     </h3>
-  //   );
-  // }
+  const handleSubmitProjectForm = () => {
+    console.log("hello");
+    // setTitle(title);
+    setEditing(false);
+  };
+  // const handleSaveClick = () => {
+  //   handleSubmitProjectForm();
+  //   setEditing(false);
+  // };
+  console.log(title);
+
+  if (!editing) {
+    return (
+      <h3 className="flex items-center gap-1">
+        Project {project.id} - {title || "Untitled"}
+        <Button
+          variant="ghost"
+          className="p-2 text-muted-foreground"
+          onClick={() => setEditing(true)}
+        >
+          <LucidePencil className="fg-muted w-4 p-0" />
+        </Button>
+      </h3>
+    );
+  }
 
   return (
-    <h3 className="flex items-center gap-1">
-      <Input defaultValue={title} disabled={!editing}
-      className="text-2xl text-black disabled:text-black"
+    <h3 className="flex items-center">
+      <span className="whitespace-nowrap">Project {project.id} - </span>
+      <Input
+        defaultValue={title}
+        disabled={!editing}
+        className="text-2xl text-black disabled:text-black"
+        onChange={(e) => setTitle(e.target.value)}
       />
       <Button
         variant="ghost"
         className="p-2 text-muted-foreground"
         onClick={() => setEditing(!editing)}
+        disabled
       >
         <LucidePencil className="fg-muted w-4 p-0 opacity-40 hover:opacity-100" />
+      </Button>
+      <Button
+        variant="ghost"
+        className="p-2 text-muted-foreground"
+        onClick={handleSubmitProjectForm}
+      >
+        <CheckSquare className="fg-muted w-4 p-0" />
+      </Button>
+      <Button
+        variant="ghost"
+        className="p-2 text-muted-foreground"
+        onClick={() => setEditing(!editing)}
+      >
+        <XSquare className="fg-muted w-4 p-0" />
       </Button>
     </h3>
   );
