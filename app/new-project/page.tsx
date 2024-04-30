0a010990d33a12ccad80e40d69115ee73d36e761
patrick <p.ch@live.co.uk>
2024-04-30 23:36:45 +0800

    prettify new-project page

--- app/new-project/page.tsx
+++ app/new-project/page.tsx
@@ -1,26 +1,48 @@
-"use client"
+"use client";
 
-import { useMemo, useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useSearchParams } from 'next/navigation';
-import { FieldValues, useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { ChevronDown, Loader2 } from 'lucide-react';
-import { CenteredLayout } from '@/components/page-layouts';
-import { Button } from "@/components/ui/button"
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+import { useMemo, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChevronDown, Loader2 } from "lucide-react";
+import { CenteredLayout } from "@/components/page-layouts";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { countries } from "content/countries";
-import { buildingTypes } from 'content/buildingTypes';
-import { submitProjectForm2 } from '@/lib/actions';
-import { NewProjectFormSchema, NewProjectFormSchemaType, NewProjectFormType } from '@/lib/types';
-import { questions } from 'content/questions';
-import { cn } from '@/lib/utils';
+import { buildingTypes } from "content/buildingTypes";
+import { submitProjectForm2 } from "@/lib/actions";
+import {
+  NewProjectFormSchema,
+  NewProjectFormSchemaType,
+  NewProjectFormType,
+} from "@/lib/types";
+import { questions } from "content/questions";
+import { cn } from "@/lib/utils";
 
-function ProjectTitle({ form }:{ form: NewProjectFormType }) {
+function ProjectTitle({ form }: { form: NewProjectFormType }) {
   return (
     <FormField
       control={form.control}
@@ -29,17 +51,20 @@ function ProjectTitle({ form }:{ form: NewProjectFormType }) {
         <FormItem>
           <FormLabel>Project Title/Owner</FormLabel>
           <FormControl>
-            <Input onChange={field.onChange} name={field.name} placeholder='Untitled'/>
+            <Input
+              onChange={field.onChange}
+              name={field.name}
+              placeholder="Untitled"
+            />
           </FormControl>
           <FormMessage />
         </FormItem>
       )}
     />
-  )
+  );
 }
 
 function CountrySelector({ form }: { form: NewProjectFormType }) {
-
   const displayNames = useMemo(() => {
     return new Intl.DisplayNames(["en"], { type: "region" });
   }, []);
@@ -51,14 +76,22 @@ function CountrySelector({ form }: { form: NewProjectFormType }) {
       render={({ field }) => (
         <FormItem>
           <FormLabel>Country</FormLabel>
-          <Select value={field.value} onValueChange={field.onChange} name={field.name}>
+          <Select
+            value={field.value}
+            onValueChange={field.onChange}
+            name={field.name}
+          >
             <FormControl>
               <SelectTrigger>
                 <SelectValue placeholder="Select a country" />
               </SelectTrigger>
             </FormControl>
             <SelectContent>
-              {countries.map((c) => <SelectItem key={c} value={c}>{displayNames.of(c)}</SelectItem>)}
+              {countries.map((c) => (
+                <SelectItem key={c} value={c}>
+                  {displayNames.of(c)}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
           <FormMessage />
@@ -69,9 +102,9 @@ function CountrySelector({ form }: { form: NewProjectFormType }) {
 }
 
 function BuildingTypeSelector({ form }: { form: NewProjectFormType }) {
-
   const curBuildingType: string = form.watch("buildingType");
-  const curBuildingSubtypeList: string[] | null = buildingTypes[curBuildingType]?.subtypes;
+  const curBuildingSubtypeList: string[] | null =
+    buildingTypes[curBuildingType]?.subtypes;
 
   return (
     <div className="flex flex-row space-x-4">
@@ -83,13 +116,15 @@ function BuildingTypeSelector({ form }: { form: NewProjectFormType }) {
             <FormLabel>Building Industry</FormLabel>
             <Select onValueChange={field.onChange} name={field.name}>
               <FormControl>
-                <SelectTrigger >
-                  <SelectValue placeholder="Select a category"/>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a category" />
                 </SelectTrigger>
               </FormControl>
               <SelectContent>
                 {Object.entries(buildingTypes).map(([k, v], i) => (
-                  <SelectItem key={i} value={k}>{v.type}</SelectItem>
+                  <SelectItem key={i} value={k}>
+                    {v.type}
+                  </SelectItem>
                 ))}
               </SelectContent>
             </Select>
@@ -101,17 +136,28 @@ function BuildingTypeSelector({ form }: { form: NewProjectFormType }) {
         control={form.control}
         name="buildingSubtype"
         render={({ field }) => (
-          <FormItem className={cn("w-1/2 transition-opacity duration-100", curBuildingSubtypeList ? "" : "opacity-0 invisible")}>
+          <FormItem
+            className={cn(
+              "w-1/2 transition-opacity duration-100",
+              curBuildingSubtypeList ? "" : "opacity-0 invisible",
+            )}
+          >
             <FormLabel>Building Type</FormLabel>
-            <Select name={field.name} defaultValue={field.value} onValueChange={field.onChange}>
+            <Select
+              name={field.name}
+              defaultValue={field.value}
+              onValueChange={field.onChange}
+            >
               <FormControl>
-                <SelectTrigger >
+                <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
               </FormControl>
               <SelectContent>
                 {curBuildingSubtypeList?.map((t, i) => (
-                  <SelectItem key={i} value={t}>{t}</SelectItem>
+                  <SelectItem key={i} value={t}>
+                    {t}
+                  </SelectItem>
                 ))}
               </SelectContent>
             </Select>
@@ -123,7 +169,7 @@ function BuildingTypeSelector({ form }: { form: NewProjectFormType }) {
   );
 }
 
-function ProjectDescription({ form }:{ form: NewProjectFormType }) {
+function ProjectDescription({ form }: { form: NewProjectFormType }) {
   return (
     <FormField
       control={form.control}
@@ -142,10 +188,10 @@ function ProjectDescription({ form }:{ form: NewProjectFormType }) {
         </FormItem>
       )}
     />
-  )
+  );
 }
 
-function ProjectDocuments({ form }:{ form: NewProjectFormType }) {
+function ProjectDocuments({ form }: { form: NewProjectFormType }) {
   return (
     <FormField
       control={form.control}
@@ -160,10 +206,10 @@ function ProjectDocuments({ form }:{ form: NewProjectFormType }) {
         </FormItem>
       )}
     />
-  )
+  );
 }
 
-function DetailedQuestion({ form, qa }:{ form: NewProjectFormType, qa : any }) {
+function DetailedQuestion({ form, qa }: { form: NewProjectFormType; qa: any }) {
   return (
     <FormField
       control={form.control}
@@ -172,9 +218,11 @@ function DetailedQuestion({ form, qa }:{ form: NewProjectFormType, qa : any }) {
         <FormItem>
           <FormLabel>{qa.title}</FormLabel>
           {/* <FormDescription> */}
-            <ul className="list-disc list-inside text-sm">
-              {qa.hints.map((hint: string[], i: number) => (<li key={i}>{hint}</li>))}
-            </ul>
+          <ul className="list-disc list-inside text-sm">
+            {qa.hints.map((hint: string[], i: number) => (
+              <li key={i}>{hint}</li>
+            ))}
+          </ul>
           {/* </FormDescription> */}
           <FormControl>
             <Textarea
@@ -187,24 +235,17 @@ function DetailedQuestion({ form, qa }:{ form: NewProjectFormType, qa : any }) {
         </FormItem>
       )}
     />
-  )
-
+  );
 }
 
-function DetailedQuestions({ form }:{ form: NewProjectFormType }) {
+function DetailedQuestions({ form }: { form: NewProjectFormType }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className="space-y-2"
-    >
+    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="space-y-2">
       <CollapsibleTrigger asChild>
         <div className="flex items-center justify-between">
-          <h4 className="text-sm font-semibold">
-            Detailed Questions
-          </h4>
+          <h4 className="text-sm font-semibold">Detailed Questions</h4>
           <Button type="button" variant="ghost" size="sm" className="w-9 p-0">
             <ChevronDown className="h-4 w-4" />
             <span className="sr-only">Toggle</span>
@@ -212,29 +253,35 @@ function DetailedQuestions({ form }:{ form: NewProjectFormType }) {
         </div>
       </CollapsibleTrigger>
       <CollapsibleContent className="space-y-2">
-        {questions.map((qa, i) =>
-          <DetailedQuestion form={form} qa={qa} key={i} />)}
+        {questions.map((qa, i) => (
+          <DetailedQuestion form={form} qa={qa} key={i} />
+        ))}
       </CollapsibleContent>
     </Collapsible>
-  )
+  );
 }
 
 function NewProjectForm() {
   const session = useSession();
 
   const searchParams = useSearchParams();
-  const countryParam: string | null = searchParams.get('country');
-  const defaultCountry: string | undefined = countries.find(c => c == countryParam?.toUpperCase());
+  const countryParam: string | null = searchParams.get("country");
+  const defaultCountry: string | undefined = countries.find(
+    (c) => c == countryParam?.toUpperCase(),
+  );
 
   // TODO save form data to local storage so it's not lost on refresh
   const form = useForm<NewProjectFormSchemaType>({
     resolver: zodResolver(NewProjectFormSchema),
     defaultValues: {
       country: defaultCountry,
-    }
-  })
+    },
+  });
 
-  async function handleSubmitProjectForm(data: FieldValues, event?: React.BaseSyntheticEvent) {
+  async function handleSubmitProjectForm(
+    data: FieldValues,
+    event?: React.BaseSyntheticEvent,
+  ) {
     const fdata = new FormData(event?.target);
     const submitSuccess = await submitProjectForm2(fdata);
     if (submitSuccess === null) {
@@ -243,11 +290,16 @@ function NewProjectForm() {
   }
 
   // if submit is successful we should redirect to the project page
-  const isWaitingRedirect = form.formState.isSubmitting || (form.formState.isSubmitted && form.formState.isSubmitSuccessful);
+  const isWaitingRedirect =
+    form.formState.isSubmitting ||
+    (form.formState.isSubmitted && form.formState.isSubmitSuccessful);
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmitProjectForm)} className="space-y-6" >
+      <form
+        onSubmit={form.handleSubmit(handleSubmitProjectForm)}
+        className="space-y-6"
+      >
         <ProjectTitle form={form} />
         <CountrySelector form={form} />
         <BuildingTypeSelector form={form} />
@@ -255,14 +307,23 @@ function NewProjectForm() {
         <ProjectDocuments form={form} />
         <DetailedQuestions form={form} />
         <div className="mt-6 flex items-center justify-end gap-x-3">
-          <Button type="submit" disabled={isWaitingRedirect} className='flex flex-row gap-3'>
+          <Button
+            type="submit"
+            disabled={isWaitingRedirect}
+            className="flex flex-row gap-3"
+          >
             {session.data?.user ? "Submit" : "Signin to Submit"}
-            <Loader2 className={cn('animate-spin h-4', isWaitingRedirect ? '' : 'hidden')}/>
+            <Loader2
+              className={cn(
+                "animate-spin h-4",
+                isWaitingRedirect ? "" : "hidden",
+              )}
+            />
           </Button>
         </div>
       </form>
     </Form>
-  )
+  );
 }
 
 export default function Page() {
@@ -275,5 +336,5 @@ export default function Page() {
         </div>
       </section>
     </CenteredLayout>
-  )
+  );
 }
