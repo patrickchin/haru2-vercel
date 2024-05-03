1e62945f41824c10d2be23c53deec51589e26289
patrick <p.ch@live.co.uk>
2024-05-04 02:34:25 +0800

    update drizzle config for local pgsql use

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "drizzle-kit";
 
 // needed for drizzle studio
 require("dotenv").config({ path: [".env.local"] });
+console.log("Using SSL", process.env.POSTGRES_SSL, process.env.POSTGRES_SSL != "false");
 
 export default {
   schema: "./drizzle/schema.ts",
@@ -14,6 +15,6 @@ export default {
     host: process.env.POSTGRES_HOST!,
     port: 5432,
     database: process.env.POSTGRES_DATABASE!,
-    ssl: true,
+    ssl: process.env.POSTGRES_SSL != "false",
   },
 } satisfies Config;
