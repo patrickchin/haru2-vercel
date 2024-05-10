0ffffdd9926dded7ad416ce13ed5ddb531527ed2
patrick <p.ch@live.co.uk>
2024-05-11 05:37:22 +0800

    delete newly created team

--- app/project/[id]/manage/components/manage-team-add-team.tsx
+++ app/project/[id]/manage/components/manage-team-add-team.tsx
@@ -3,28 +3,43 @@
 import { Button } from "@/components/ui/button";
 import { LucidePlus } from "lucide-react";
 import * as Actions from "@/lib/actions";
-import { revalidatePath } from "next/cache";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { teamNames } from "@/lib/types";
+import { useState } from "react";
 
 export default function AddTeamMemberButton({
   projectId,
 }: {
   projectId: number;
 }) {
-  const pathname = usePathname();
+  const [selectedTeamType, setSelectedTeamType] = useState("");
   const router = useRouter();
   return (
-    <Button
-      variant="outline"
-      onClick={async () => {
-        const newTeam = await Actions.createProjectTeam(projectId, "legal");
-        console.log(newTeam);
-        // revalidatePath(pathname); // doesn't work
-        router.refresh();
-      }}
-    >
-      <LucidePlus className="h-4" />
-      Add Team
-    </Button>
+    <div className="flex gap-2 font-semibold">
+      <Select onValueChange={setSelectedTeamType}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a team type" />
+        </SelectTrigger>
+        <SelectContent>
+          {Object.entries(teamNames).map(([k, v]) => (
+            <SelectItem key={k} value={k}>{v}</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Button
+        variant="outline"
+        onClick={async () => {
+          if (selectedTeamType.length > 0) {
+            await Actions.createProjectTeam(projectId, selectedTeamType);
+          }
+          // revalidatePath(pathname); // doesn't work
+          router.refresh();
+        }}
+      >
+        <LucidePlus className="h-4" />
+        Add Team
+      </Button>
+    </div>
   );
 }
