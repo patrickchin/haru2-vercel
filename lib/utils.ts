823a3c026d2441ae2a95fd4d4935d65ce4e10d52
Binayak Ray <csebinayak.ku@gmail.com>
2024-05-12 17:22:49 +0600

    use at method for getAvatar initials

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,21 +5,21 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getAvatarInitials(fullName: string) {
+export function getAvatarInitials(fullName: string | null | undefined): string {
+  if (!fullName) return "";
   const names = fullName.trim().split(/\s+/);
   let initials = "";
 
   if (names.length === 1) {
-    // If there's only one name, use the first and last character of this name
-    initials =
-      names[0][0] +
-      (names[0].length > 1 ? names[0][names[0].length - 1] : names[0][0]);
-  } else {
-    // Use the first character of the first name and the last character of the last name
-    initials =
-      names[0][0] + names[names.length - 1][names[names.length - 1].length - 1];
+    initials = names[0].slice(0, 2);
+  } else if (names.length > 1) {
+    const lastName = names.at(-1);
+    if (lastName) {
+      initials = names[0].slice(0, 2) + lastName[0];
+    } else {
+      initials = names[0].slice(0, 2);
+    }
   }
 
-  // Convert initials to uppercase
   return initials.toUpperCase();
 }
