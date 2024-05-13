0628a55c46490f59226e7c51be4b4a565d32ce7b
patrick <p.ch@live.co.uk>
2024-05-13 22:57:14 +0800

    small changes to getAvatarInitials

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,21 +5,16 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getAvatarInitials(fullName: string | null | undefined): string {
-  if (!fullName) return "";
-  const names = fullName.trim().split(/\s+/);
-  let initials = "";
+export function getAvatarInitials(fullname: string | null | undefined): string {
+  if (!fullname) return "?";
+  const names = fullname.trim().split(/\s+/);
+  const firstname = names.at(0);
+  if (!firstname) return "?";
 
-  if (names.length === 1) {
-    initials = names[0].slice(0, 2);
-  } else if (names.length > 1) {
-    const lastName = names.at(-1);
-    if (lastName) {
-      initials = names[0].slice(0, 2) + lastName[0];
-    } else {
-      initials = names[0].slice(0, 2);
-    }
-  }
+  let initials = firstname.slice(0, 2);
+  const lastname = names.at(-1);
+  if (lastname && names.length > 1)
+    initials = firstname[0] + lastname[0];
 
   return initials.toUpperCase();
 }
