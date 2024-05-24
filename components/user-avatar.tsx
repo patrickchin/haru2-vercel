dd415f0a8b6bd3df76bec656f14e5cf85ded9fe9
patrick <p.ch@live.co.uk>
2024-05-24 17:45:28 +0800

    use user id to generate a colour for avatar

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -3,29 +3,6 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { cn, getAvatarInitials } from "@/lib/utils";
 import { User } from "next-auth";
 
-export function UserAvatar({
-  user,
-  className,
-}: {
-  // user: DesignUserBasic;
-  user?: User;
-  className?: string;
-}) {
-  return (
-    <Avatar className={cn("", className)}>
-      {/* <AvatarImage src={user.avatarUrl ?? undefined} /> */}
-      <AvatarImage src={user?.image ?? undefined} />
-      <AvatarFallback
-        // style={{ backgroundColor: user.avatarColor ?? "lightblue" }}
-        style={{ backgroundColor: "lightblue" }}
-        className="text-xl text-align-center"
-      >
-        {getAvatarInitials(user?.name ?? undefined)}
-      </AvatarFallback>
-    </Avatar>
-  );
-}
-
 export function DesignUserAvatar({
   user,
   className,
@@ -33,15 +10,44 @@ export function DesignUserAvatar({
   user: DesignUserBasic;
   className?: string;
 }) {
+  // const userId: number = Math.floor(Math.random() * 1000);
+  const userId: number = user?.id ?? 0;
+  const randomHue: number = ((userId-1) * 80) % 360;
+  const bgCol: string = `hsl(${randomHue} 100 75)`;
+  const bgCol2: string = `hsl(${randomHue+60} 100 90)`;
   return (
-    <Avatar className={cn("", className)}>
-      <AvatarImage src={user.avatarUrl ?? undefined} />
+    <Avatar className={cn("border border-foreground", className)}>
+      <AvatarImage src={user?.avatarUrl ?? undefined} />
       <AvatarFallback
-        style={{ backgroundColor: user.avatarColor ?? "lightblue" }}
-        className="text-xl text-align-center"
+        style={{
+          backgroundColor: bgCol,
+          backgroundImage: `radial-gradient(circle at top left, ${bgCol2}, ${bgCol})`,
+        }}
+        className="text-lg text-align-center"
       >
         {getAvatarInitials(user.name ?? undefined)}
       </AvatarFallback>
     </Avatar>
+  );
+}
+
+export function UserAvatar({
+  user,
+  className,
+}: {
+  user?: User;
+  className?: string;
+}) {
+  return (
+    <DesignUserAvatar
+      className={className}
+      user={{
+        id: Number(user?.id),
+        name: user?.name ?? "?",
+        email: "",
+        avatarUrl: user?.image ?? null,
+        avatarColor: "",
+      }}
+    />
   );
 }
