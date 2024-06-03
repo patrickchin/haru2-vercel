523046d2443597349ee0d803393279982571002b
binayak20 <csebinayak.ku@gmail.com>
2024-06-03 15:50:11 +0600

    add some git ignor for local environment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -13,8 +13,8 @@ export default {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST!,
-    port: 5432,
+    port: 5433,
     database: process.env.POSTGRES_DATABASE!,
-    ssl: process.env.POSTGRES_SSL != "false",
+    ssl: process.env.POSTGRES_SSL !== "false",
   },
 } satisfies Config;
