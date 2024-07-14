5e57bb65c5999eb287280b45bc918fa987d4ac37
patrick <p.ch@live.co.uk>
2024-07-15 02:19:19 +0800

    Cleanup api/upload route

--- app/api/upload/[type]/route.ts
+++ app/api/upload/[type]/route.ts
@@ -1,10 +1,51 @@
 import { NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
+import { Session } from "next-auth";
 import { createPresigned } from "@/lib/s3";
+import { auth } from "@/lib/auth";
 import * as Actions from "@/lib/actions";
 
 // is this even still maintained??
 import sanitizeFilename from "sanitize-filename";
+
+function getAvatarPath(session: Session, filename: string) {
+  const userId = Number(session.user?.id);
+  if (isNaN(userId)) return;
+  return `user/${userId}/${filename}`;
+}
+
+async function getTaskFilePath(
+  params: Record<string, string>,
+  filename: string,
+) {
+  if (!params.taskId) return;
+  const task = await Actions.getTask(Number(params.taskId));
+  if (!task || !task.projectid || !task.specid) return;
+  return `project/${task.projectid}/task/${task.specid}/${filename}`;
+}
+
+async function getProjectFilePath(
+  params: Record<string, string>,
+  filename: string,
+) {
+  if (!params.projectId) return;
+  const project = await Actions.getProject(Number(params.projectId));
+  if (!project) return;
+  return `project/${project.id}/${filename}`;
+}
+
+async function getPath(session: Session, params: Record<string, string>) {
+  const unsanFilename = params.filename;
+  if (!unsanFilename || unsanFilename.length <= 0) return;
+  const filename = sanitizeFilename(unsanFilename);
+
+  if (params.type === "avatar") {
+    return getAvatarPath(session, filename);
+  } else if (params.type === "task") {
+    return getTaskFilePath(params, filename);
+  } else if (params.type === "project") {
+    return getProjectFilePath(params, filename);
+  }
+}
 
 export async function POST(
   request: Request,
@@ -19,35 +60,9 @@ export async function POST(
     );
   }
 
-  const invalidParamsResponse = NextResponse.json(
-    { error: "Invalid Params" },
-    { status: 400 },
-  );
-
-  const unsanFilename = params.filename as string;
-  if (!unsanFilename || unsanFilename.length <= 0) return invalidParamsResponse;
-  const filename = sanitizeFilename(unsanFilename);
-
-  let path = "";
-
-  if (type === "avatar") {
-    const userId = Number(session.user?.id);
-    if (isNaN(userId)) return invalidParamsResponse;
-    path = `user/${userId}/${filename}`;
-  } else if (type === "task") {
-    const { taskId } = params;
-    if (!taskId) return invalidParamsResponse;
-    const task = await Actions.getTask(taskId);
-    if (!task || !task.projectid || !task.specid) return invalidParamsResponse;
-    path = `project/${task.projectid}/task/${task.specid}/${filename}`;
-  } else if (type === "project") {
-    const { projectId } = params;
-    if (!projectId) return invalidParamsResponse;
-    const project = await Actions.getProject(projectId);
-    if (!project) return invalidParamsResponse;
-    path = `project/${project.id}/${filename}`;
-  } else {
-    return invalidParamsResponse;
+  const path = await getPath(session, params);
+  if (!path) {
+    return NextResponse.json({ error: "Invalid Params" }, { status: 400 });
   }
 
   try {
