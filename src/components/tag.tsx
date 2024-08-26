606955b203bcd8f4bb80074cdb5d36ea878ca341
Ahmad Abdurrahman Yahaya <reachtoabdul@gmail.com>
2024-08-26 20:31:35 +0800

    update tag color

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -7,7 +7,7 @@ interface TagProps {
 
 export default function Tag({ title }: TagProps) {
   return (
-    <button className="px-4 py-2 outline rounded-full m-1.5 text-center text-sm hover:bg-black hover:text-white focus:bg-black focus:outline-none focus:text-white text-slate-700">
+    <button className="px-4 py-2 outline rounded-full m-1.5 text-center text-sm hover:bg-slate-800 hover:text-white focus:bg-slate-800 focus:outline-none focus:text-white text-slate-700 outline-slate-800">
       {title}
     </button>
   );
