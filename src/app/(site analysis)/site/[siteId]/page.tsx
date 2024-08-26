c9e7d134534d37507df9eab0753963079837a263
patrick <p.ch@live.co.uk>
2024-08-26 20:50:15 +0800

    add header to site description page

--- src/app/(site analysis)/site/[siteId]/page.tsx
+++ src/app/(site analysis)/site/[siteId]/page.tsx
@@ -1,15 +1,25 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Page({ params }: { params: { siteId: string } }) {
   const siteId = Number(params.siteId);
   return (
-    <>
-      <h3>Site Description Page</h3>
-      <p>Some description of hte site</p>
-      <Button>
-        <Link href={`/site/${siteId}/reports`}>Click Here to View Reports</Link>
-      </Button>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <main className="grow flex flex-col items-center px-16 py-8 gap-4">
+        <h3>Site {siteId} Description Page</h3>
+        <p>Some description of the site</p>
+        <Button>
+          <Link href={`/site/${siteId}/reports`}>
+            Click Here to View Reports
+          </Link>
+        </Button>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
