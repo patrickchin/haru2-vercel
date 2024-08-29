f4799f028a22423ef204cfc0af76caefd804ea41
patrick <p.ch@live.co.uk>
2024-08-30 03:09:40 +0800

    very basic site description page

---
+++ src/app/(site analysis)/site/[siteId]/site-calendar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import React from "react";
+
+export default function SiteCalendar() {
+const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+return (
+  <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
+);
+
+}
