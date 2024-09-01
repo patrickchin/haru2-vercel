258ce4781e816d6b22dea5d7a62ca4a22a34ce65
patrick <p.ch@live.co.uk>
2024-09-02 06:15:51 +0800

    sites list tweaks

--- src/app/(site analysis)/sites/page.tsx
+++ src/app/(site analysis)/sites/page.tsx
@@ -5,6 +5,7 @@ import * as Actions from "@/lib/actions";
 import { Button } from "@/components/ui/button";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { LucideArrowRight } from "lucide-react";
 
 function EmptySitesList() {
   return <p>You currently do not have any sites registered with us</p>;
@@ -18,34 +19,42 @@ async function SitesList() {
   }
 
   return (
-    <ol>
+    <ol className="w-96 flex flex-col gap-4">
       {sites?.map((site, i) => {
         return (
           <li key={i}>
-            <Button asChild variant="outline">
-              <Link href={`/site/${site.id}`}>Site {site.id}</Link>
-            </Button>
+            <Link
+              href={`/site/${site.id}`}
+              className="flex items-center gap-3 p-4 border hover:bg-accent"
+            >
+              <div className="grow flex flex-col">
+                <p>
+                  Site {site.id}: {site.title}
+                </p>
+                <p>{site.createdAt?.toDateString()}</p>
+              </div>
+              <Button variant="secondary">
+                <LucideArrowRight className="w-3.5" />
+              </Button>
+            </Link>
           </li>
         );
       })}
     </ol>
   );
-
 }
 
 export default async function Page() {
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
-      <main className="grow flex flex-col items-center px-16 py-8 gap-4">
+      <main className="grow flex flex-col items-center px-16 py-8 gap-8">
+        <h2>My Site Supervision Projects</h2>
         <Button asChild>
           <Link href="/new-site">Register New Construction Site</Link>
         </Button>
         <SitesList />
       </main>
-
       <Footer />
     </div>
   );
