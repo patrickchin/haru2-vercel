231811e05cbc500dbdc510214efbd257c88db983
patrick <p.ch@live.co.uk>
2024-09-05 14:24:47 +0800

    site description add submit form for a zoom meeting

--- src/app/(site analysis)/site/[siteId]/site-calendar.tsx
+++ src/app/(site analysis)/site/[siteId]/site-calendar.tsx
@@ -1,18 +1,128 @@
-"use client";
+"use client"
 
-import { Calendar } from "@/components/ui/calendar";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { format } from "date-fns"
+import { LucideCalendar } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-export default function SiteCalendar() {
-const [date, setDate] = React.useState<Date | undefined>(new Date())
+import { cn } from "@/lib/utils"
+import { toast } from "@/components/ui/use-toast"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Input } from "@/components/ui/input"
 
-return (
-  <Calendar
-    mode="single"
-    selected={date}
-    onSelect={setDate}
-    className="rounded-md border"
-  />
-);
+const FormSchema = z.object({
+  dates: z
+    .date({
+      required_error: "Pick a date ",
+    })
+    .optional(),
+  desc: z.string().optional(),
+});
 
+export default function CalendarForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
+  const dateNow = new Date();
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="dates"
+          render={({ field }) => (
+            <FormItem className="grow flex flex-col">
+              <FormLabel>Meeting Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "pl-3 font-normal",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <LucideCalendar className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date < new Date(dateNow.getTime() - 24 * 60 * 60_000) ||
+                      date > new Date(dateNow.getTime() + 32 * 24 * 60 * 60_000)
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="desc"
+          render={({ field }) => (
+            <FormItem className="">
+              <FormLabel className="">Extra Info</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Specify your prefered times" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex justify-end">
+          <Button type="submit">Add a Date</Button>
+        </div>
+      </form>
+    </Form>
+  );
 }
+
+
+
