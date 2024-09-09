25d27cbd20488bf248d66db0725f91fea7b48a32
patrick <p.ch@live.co.uk>
2024-09-10 03:18:58 +0800

    site description display some suggested meeting times

--- src/app/(site analysis)/site/[siteId]/site-calendar.tsx
+++ src/app/(site analysis)/site/[siteId]/site-calendar.tsx
@@ -1,30 +1,29 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { format } from "date-fns"
-import { LucideCalendar } from "lucide-react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { LucideCalendar, LucidePlus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { cn } from "@/lib/utils"
-import { toast } from "@/components/ui/use-toast"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils";
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/popover";
+import { Input } from "@/components/ui/input";
 
 const FormSchema = z.object({
   dates: z
@@ -38,7 +37,7 @@ const FormSchema = z.object({
 export default function CalendarForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-  })
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     toast({
@@ -48,7 +47,7 @@ export default function CalendarForm() {
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-    })
+    });
   }
 
   const dateNow = new Date();
@@ -57,72 +56,74 @@ export default function CalendarForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-4"
+        className="flex flex-row gap-4"
       >
-        <FormField
-          control={form.control}
-          name="dates"
-          render={({ field }) => (
-            <FormItem className="grow flex flex-col">
-              <FormLabel>Meeting Date</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "pl-3 font-normal",
-                        !field.value && "text-muted-foreground",
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                      <LucideCalendar className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date < new Date(dateNow.getTime() - 24 * 60 * 60_000) ||
-                      date > new Date(dateNow.getTime() + 32 * 24 * 60 * 60_000)
-                    }
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="grid grid-cols-2 gap-3 items-baseline">
+          <FormField
+            control={form.control}
+            name="dates"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Meeting Date</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "pl-3 font-normal w-full",
+                          !field.value && "text-muted-foreground",
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <LucideCalendar className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="center">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date < new Date(dateNow.getTime() - 24 * 60 * 60_000) ||
+                        date >
+                          new Date(dateNow.getTime() + 32 * 24 * 60 * 60_000)
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="desc"
-          render={({ field }) => (
-            <FormItem className="">
-              <FormLabel className="">Extra Info</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder="Specify your prefered times" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="desc"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel className="">Meeting Time</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Specify your prefered times" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
 
-        <div className="flex justify-end">
-          <Button type="submit">Add a Date</Button>
+        <div className="flex items-end">
+          <Button type="submit">
+            <LucidePlus className="w-4" />
+          </Button>
         </div>
       </form>
     </Form>
   );
 }
-
-
-
