b18b6b9895dd1c459ceee07f47fdc44c7985a47e
patrick <p.ch@live.co.uk>
2024-09-30 00:31:32 +0800

    update migration files for site meetings tabls

--- src/lib/db/drizzle/0001_kind_rachel_grey.sql
+++ src/lib/db/drizzle/0001_kind_rachel_grey.sql
@@ -1,5 +1,5 @@
 DO $$ BEGIN
- CREATE TYPE "public"."siteMeetingStatus" AS ENUM('pending', 'confirmed', 'cancelled');
+ CREATE TYPE "public"."siteMeetingStatus" AS ENUM('pending', 'rejected', 'confirmed', 'cancelled');
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -7,6 +7,7 @@ END $$;
 CREATE TABLE IF NOT EXISTS "siteMeetings1" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"siteId" integer,
+	"userId" integer,
 	"status" "siteMeetingStatus" DEFAULT 'pending',
 	"date" timestamp with time zone,
 	"duration" interval,
@@ -16,6 +17,12 @@ CREATE TABLE IF NOT EXISTS "siteMeetings1" (
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "siteMeetings1" ADD CONSTRAINT "siteMeetings1_siteId_sites1_id_fk" FOREIGN KEY ("siteId") REFERENCES "public"."sites1"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "siteMeetings1" ADD CONSTRAINT "siteMeetings1_userId_users1_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users1"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
