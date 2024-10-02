6b9ebb1f503770186da69c69ceefb9365ab91ddd
patrick <p.ch@live.co.uk>
2024-10-02 12:55:46 +0800

    indicatorClassName for progress-bar

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -7,13 +7,13 @@ import { cn } from "@/lib/utils"
 
 interface CustomProgressProps
   extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
-  indicatorColor?: string;
+  indicatorClassName?: string;
 }
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   CustomProgressProps
->(({ className, indicatorColor = "bg-primary", value, ...props }, ref) => (
+>(({ className, indicatorClassName = "bg-primary", value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -23,7 +23,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn("h-full w-full flex-1 transition-all", indicatorColor)}
+      className={cn("h-full w-full flex-1 transition-all", indicatorClassName)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
