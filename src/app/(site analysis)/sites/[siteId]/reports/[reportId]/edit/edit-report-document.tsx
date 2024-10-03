3e60a42fe8609a6ae002e7af777fa7a1ba54f912
Ahmad Abdurrahman Yahaya <reachtoabdul@gmail.com>
2024-10-03 19:20:20 +0800

    add form to supervisor-edit-report-page

---
+++ src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/edit-report-document.tsx
@@ -0,0 +1,295 @@
+"use client";
+import { cn } from "@/lib/utils";
+
+import { LucideChevronDown, LucideMoveLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  SiteDetails,
+  SiteReport,
+  SiteReportBoth,
+  SiteReportSection,
+} from "@/lib/types/site";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Form, FormField, FormItem } from "@/components/ui/form";
+import { FormControl, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+export const currentActivitesSchema = z.object({
+  activity: z.string(),
+  contractors: z.string(),
+  engineers: z.string(),
+  workers: z.string(),
+  visitors: z.string(),
+  materialsUsed: z.string(),
+  equipmentsUsed: z.string(),
+});
+export type CurrentActivitiesType = z.infer<typeof currentActivitesSchema>;
+
+export const estimatedBudgetAndTimelineSchema = z.object({
+  estimatedBudget: z.string(),
+  constructionTimeline: z.string(),
+  budgetSpent: z.string(),
+  completionDate: z.string(),
+});
+export type EstimatedBudgetAndTimelineType = z.infer<
+  typeof estimatedBudgetAndTimelineSchema
+>;
+
+export async function EditReportDocument({
+  site,
+  report,
+  sections,
+}: {
+  site?: SiteDetails;
+  report?: SiteReportBoth;
+  sections?: SiteReportSection[];
+}) {
+  const estimatedBudgetAndTimelineForm =
+    useForm<EstimatedBudgetAndTimelineType>({
+      resolver: zodResolver(estimatedBudgetAndTimelineSchema),
+    });
+  const currentActivitiesForm = useForm<CurrentActivitiesType>({
+    resolver: zodResolver(currentActivitesSchema),
+  });
+
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-4",
+        site && report ? "" : "brightness-95",
+      )}
+    >
+      <Card className="bg-yellow-50 border-2">
+        <CardHeader className="flex flex-row justify-between">
+          <div className="text-lg font-bold">
+            Current Budget and Timeline Estimates
+          </div>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4 p-4 pt-0">
+          <Form {...estimatedBudgetAndTimelineForm}>
+            <form className="w-full">
+              <div className="grid grid-cols-2 gap-6 w-full">
+                <FormField
+                  control={estimatedBudgetAndTimelineForm.control}
+                  name="estimatedBudget"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Construction Budget</FormLabel>
+                      <FormControl>
+                        <Input
+                          name={field.name}
+                          className="w-full"
+                          placeholder="eg. $2,000,000.00"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={estimatedBudgetAndTimelineForm.control}
+                  name="constructionTimeline"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Construction Timeline</FormLabel>
+                      <FormControl>
+                        <Input
+                          name={field.name}
+                          className=""
+                          placeholder="eg. 215 days"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={estimatedBudgetAndTimelineForm.control}
+                  name="budgetSpent"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Budget Spent</FormLabel>
+                      <FormControl>
+                        <Input
+                          name={field.name}
+                          className=""
+                          placeholder="eg. $200,000.00"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={estimatedBudgetAndTimelineForm.control}
+                  name="completionDate"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Completion Date</FormLabel>
+                      <FormControl>
+                        <Input
+                          name={field.name}
+                          className=""
+                          placeholder="eg. 02/02/2029"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button className="w-full mt-6">Save</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+      <Card className="bg-cyan-50 border-2">
+        <CardHeader className="flex flex-row justify-between">
+          <div className="text-lg font-bold">
+            Current Construction Activites
+          </div>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4 p-4 pt-0">
+          <Form {...currentActivitiesForm}>
+            <form className="w-full">
+              <div className="basis-1/4 border p-4 bg-background space-y-2">
+                <h6>Site Activity</h6>
+                <FormField
+                  control={currentActivitiesForm.control}
+                  name="activity"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          name={field.name}
+                          className="w-full"
+                          placeholder="eg. Excavation"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="basis-1/4 border p-4 bg-background space-y-2 mt-4">
+                <h6>Site Personel</h6>
+
+                <div>
+                  <FormField
+                    control={currentActivitiesForm.control}
+                    name="contractors"
+                    render={({ field }) => (
+                      <FormItem className="mt-2">
+                        <FormLabel>Contractors</FormLabel>
+                        <FormControl>
+                          <Input
+                            name={field.name}
+                            className=""
+                            placeholder="eg. John Doe"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={currentActivitiesForm.control}
+                    name="engineers"
+                    render={({ field }) => (
+                      <FormItem className="mt-2">
+                        <FormLabel>Engineers</FormLabel>
+                        <FormControl>
+                          <Input
+                            name={field.name}
+                            className=""
+                            placeholder="eg. John Doe"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={currentActivitiesForm.control}
+                    name="workers"
+                    render={({ field }) => (
+                      <FormItem className="mt-2">
+                        <FormLabel>Workers</FormLabel>
+                        <FormControl>
+                          <Input
+                            name={field.name}
+                            className=""
+                            placeholder="eg. John Doe"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={currentActivitiesForm.control}
+                    name="visitors"
+                    render={({ field }) => (
+                      <FormItem className="mt-2">
+                        <FormLabel>Visitors</FormLabel>
+                        <FormControl>
+                          <Input
+                            name={field.name}
+                            className=""
+                            placeholder="eg. John Doe"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="basis-1/4 border p-4 bg-background space-y-2  mt-4">
+                <h6>Materials Used</h6>
+                <FormField
+                  control={currentActivitiesForm.control}
+                  name="materialsUsed"
+                  render={({ field }) => (
+                    <FormItem className="mt-2">
+                      <FormControl>
+                        <Textarea
+                          name={field.name}
+                          className=""
+                          placeholder="Type here"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="basis-1/4 border p-4 bg-background space-y-2  mt-4">
+                <h6>Equiptment Used</h6>
+                <FormField
+                  control={currentActivitiesForm.control}
+                  name="equipmentsUsed"
+                  render={({ field }) => (
+                    <FormItem className="mt-2">
+                      <FormControl>
+                        <Textarea
+                          name={field.name}
+                          className=""
+                          placeholder="Type here"
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button className="w-full mt-6">Save</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/page.tsx
+++ src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { UpdateSiteReportFiles } from "./edit-upload";
 import { UpdateSiteReportSections } from "./edit-sections";
+import { EditReportDocument } from "./edit-report-document";
 import * as Actions from "@/lib/actions";
 
 export default async function Page({
@@ -25,15 +26,9 @@ export default async function Page({
     const reportId = Number(params.reportId);
     return (
       <DefaultLayout>
-        <Button asChild>
-          <Link href={`/sites/${siteId}/reports/${reportId}`}>
-            Back To Report
-          </Link>
-        </Button>
-        <UpdateSiteReportFiles siteId={siteId} reportId={reportId} />
-        <UpdateSiteReportForm siteId={siteId} reportId={reportId} />
-        <UpdateSiteReportDetailsForm siteId={siteId} reportId={reportId} />
-        <UpdateSiteReportSections siteId={siteId} reportId={reportId} />
+        <EditReportDocument
+        // Report
+        />
       </DefaultLayout>
     );
   } else {
