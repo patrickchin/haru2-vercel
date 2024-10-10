044b4e874d6af88858fad460e62b1467888c572b
patrick <p.ch@live.co.uk>
2024-10-10 16:07:29 +0800

    separate permissions to server and client sides

---
+++ src/lib/permissions-server.ts
@@ -0,0 +1,40 @@
+import * as db from "@/lib/db";
+import { Session } from "next-auth";
+import { allSiteMemberRoles, SiteMemberRole } from "@/lib/types";
+
+export { viewingRoles } from "./permissions";
+export { editingRoles } from "./permissions";
+
+export async function siteActionAllowed(
+  session: Session | null,
+  allowedRoles: SiteMemberRole[],
+  {
+    siteId,
+    reportId,
+    sectionId,
+    meetingId,
+  }: {
+    siteId?: number;
+    reportId?: number;
+    sectionId?: number;
+    meetingId?: number;
+  },
+) {
+  if (!session?.user) return false;
+  if (session.user.role === "admin") return true;
+
+  const userId = session.user.idn;
+  let role: SiteMemberRole = null;
+  if (siteId) {
+    role = await db.getSiteRole({ siteId, userId });
+  } else if (reportId) {
+    role = await db.getReportRole({ reportId, userId });
+  } else if (sectionId) {
+    role = await db.getReportSectionRole({ sectionId, userId });
+  } else if (meetingId) {
+    role = await db.getMeetingRole({ meetingId, userId });
+  } else {
+    return false;
+  }
+  return allowedRoles.includes(role);
+}
--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,5 +1,3 @@
-import * as db from "@/lib/db";
-import { Session } from "next-auth";
 import { allSiteMemberRoles, SiteMemberRole } from "@/lib/types";
 
 export const viewingRoles: SiteMemberRole[] = allSiteMemberRoles;
@@ -8,37 +6,3 @@ export const editingRoles: SiteMemberRole[] = [
   "owner",
   "manager",
 ];
-
-export async function siteActionAllowed(
-  session: Session | null,
-  allowedRoles: SiteMemberRole[],
-  {
-    siteId,
-    reportId,
-    sectionId,
-    meetingId,
-  }: {
-    siteId?: number;
-    reportId?: number;
-    sectionId?: number;
-    meetingId?: number;
-  },
-) {
-  if (!session?.user) return false;
-  if (session.user.role === "admin") return true;
-
-  const userId = session.user.idn;
-  let role: SiteMemberRole = null;
-  if (siteId) {
-    role = await db.getSiteRole({ siteId, userId });
-  } else if (reportId) {
-    role = await db.getReportRole({ reportId, userId });
-  } else if (sectionId) {
-    role = await db.getReportSectionRole({ sectionId, userId });
-  } else if (meetingId) {
-    role = await db.getMeetingRole({ meetingId, userId });
-  } else {
-    return false;
-  }
-  return allowedRoles.includes(role);
-}
