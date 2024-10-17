c7074614d0a2b554c4df2c92ba9cc832cae6fdc7
patrick <p.ch@live.co.uk>
2024-10-18 07:22:15 +0800

    change front page bg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ArrowRightIcon } from "lucide-react";
 import Header from "@/components/header";
@@ -11,28 +12,36 @@ export default function Page() {
       <main className="grow flex flex-col justify-center items-center relative">
         <div className="grid grid-cols-1 absolute h-full w-full -z-10">
           {/* <div className="h-full bg-gradient-to-br from-cyan-100 to-indigo-200 hover:bg-none"></div> */}
-          <div className="h-full bg-gradient-to-br from-pink-100 to-indigo-200"></div>
+          {/* <div className="h-full bg-[url('/bg.jpg')] bg-"></div> */}
+          <Image
+            src="/bg.jpg"
+            alt="background"
+            fill={true}
+            className="object-cover object-left-top"
+          />
         </div>
 
-        <div className="flex flex-col gap-16 max-w-7xl">
-          <h1 className="flex text-center text-6xl font-extrabold">
+        <div className="flex flex-col gap-16 max-w-7xl lg:mb-64 lg:ml-80 justify-end transition-all">
+          <h1 className="flex text-right text-6xl whitespace-nowrap font-extrabold p-10 bg-slate-100/60 rounded">
             Your Construction Projects, <br />
             Seamlessly Supervised
           </h1>
 
-          <p className="text-center font-bold">
-            <span className="whitespace-nowrap pr-2">
-              Hire our Site Analysis Professionals
-            </span>
-            <Button asChild className="w-fit font-bold text-base">
-              <Link href="/new-site">
-                Get Started <ArrowRightIcon className="ml-2" />
-              </Link>
-            </Button>
-          </p>
+          <div className="flex justify-end">
+            <p className="text-right font-bold p-10 bg-slate-100/60 rounded">
+              <span className="whitespace-nowrap pr-2">
+                Hire our Site Analysis Professionals
+              </span>
+              <Button asChild className="w-fit font-bold text-base">
+                <Link href="/new-site">
+                  Get Started <ArrowRightIcon className="ml-2" />
+                </Link>
+              </Button>
+            </p>
+          </div>
         </div>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
