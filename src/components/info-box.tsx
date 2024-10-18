58b3da5c5c607d7c93b6e499789d2eebbf731f8b
patrick <p.ch@live.co.uk>
2024-10-19 00:34:02 +0800

    optional children in info boxes

--- src/components/info-box.tsx
+++ src/components/info-box.tsx
@@ -1,7 +1,7 @@
 import { LucideCheckCircle, LucideInfo, LucideThumbsUp } from "lucide-react";
 import { ReactNode } from "react";
 
-export function InfoBox({ children }: { children: ReactNode }) {
+export function InfoBox({ children }: { children?: ReactNode }) {
   return (
     <div className="flex gap-3 bg-blue-50 border-2 p-4 rounded border-blue-200">
       <LucideInfo className="flex-none h-6 w-6" />
@@ -12,7 +12,7 @@ export function InfoBox({ children }: { children: ReactNode }) {
   );
 }
 
-export function GoodBox({ children }: { children: ReactNode }) {
+export function GoodBox({ children }: { children?: ReactNode }) {
   return (
     <div className="flex gap-3 bg-green-50 border-2 p-4 rounded border-green-200">
       <LucideCheckCircle className="flex-none h-5 w-5" />
