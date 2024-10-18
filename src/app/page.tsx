ceaf76b524f742e032e4995b30daa7ccceb921ad
patrick <p.ch@live.co.uk>
2024-10-18 20:49:01 +0800

    fix bgblur on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,28 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { cn } from "@/lib/utils";
 
-import bgblur from "@/public/bgblur.bmp";
+const bgblur =
+  "data:image/bmp;base64," +
+  "Qk2aAwAAAAAAAIoAAAB8AAAACgAAAAcAAAABABgAAAAAAOAAAAByJgAAciYAAAAA" +
+  "AAAAAAAAAAD/AAD/AAD/AAAAAAAA/0RFQk2PwvUoUbgeFR6F6wEzMzMTZmZmJmZm" +
+  "ZgaZmZkJPQrXAyhcjzIAAAAAAAAAAAAAAAAEAAAAXAEAADACAAAAAAAAPy4sRDEt" +
+  "KRweSjw9nqq9ma/Ppq62vsXHj5OWlZygAABJNC49KyY3LTRsaXO9xMqsvtu6wsu3" +
+  "v8J/gYGSmJsAAGtYUUUxLEhFUZ2fotvf4Li5upuhqbfCzoSIjJeeogAAxcTGS0RK" +
+  "SUpdnaa30tjZt7m7nqWrs7/JeHyAjJSZAADb4OSGg4UuLkF8l7Pc3ty2ur6FjJOD" +
+  "i5JzdnltdHoAANa9qebp68TGya2zuNfa3LO2uJugpYSNknR4fH+JkgAA0p5z5Mu3" +
+  "6+flsrO21NfYtrq+r7KzoaitdXyCrbS5AAAAAAIwQURCRQIQAABtbnRyUkdCIFhZ" +
+  "WiAHzwAGAAMAAAAAAABhY3NwQVBQTAAAAABub25lAAAAAAAAAAAAAAAAAAAAAAAA" +
+  "9tYAAQAAAADTLUFEQkUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+  "AAAAAAAAAAAAAAAAAApjcHJ0AAAA/AAAADJkZXNjAAABMAAAAGt3dHB0AAABnAAA" +
+  "ABRia3B0AAABsAAAABRyVFJDAAABxAAAAA5nVFJDAAAB1AAAAA5iVFJDAAAB5AAA" +
+  "AA5yWFlaAAAB9AAAABRnWFlaAAACCAAAABRiWFlaAAACHAAAABR0ZXh0AAAAAENv" +
+  "cHlyaWdodCAxOTk5IEFkb2JlIFN5c3RlbXMgSW5jb3Jwb3JhdGVkAAAAZGVzYwAA" +
+  "AAAAAAARQWRvYmUgUkdCICgxOTk4KQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+  "AAAAAAAAWFlaIAAAAAAAAPNRAAEAAAABFsxYWVogAAAAAAAAAAAAAAAAAAAAAGN1" +
+  "cnYAAAAAAAAAAQIzAABjdXJ2AAAAAAAAAAECMwAAY3VydgAAAAAAAAABAjMAAFhZ" +
+  "WiAAAAAAAACcGAAAT6UAAAT8WFlaIAAAAAAAADSNAACgLAAAD5VYWVogAAAAAAAA" +
+  "JjEAABAvAAC+nA==";
 
 export default function Page() {
   return (
@@ -19,7 +40,7 @@ export default function Page() {
             src="/bg.jpg"
             alt="background"
             loading="eager"
-            blurDataURL={bgblur.src}
+            blurDataURL={bgblur}
             placeholder="blur"
             fill={true}
             className="object-cover object-center"
