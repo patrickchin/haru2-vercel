1544e0f02dada7b26e4fdfd6a6bba49849471add
patrick <p.ch@live.co.uk>
2024-10-31 05:49:16 +0800

    add InputDate popover

---
+++ src/components/input-date.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { formatDate } from "date-fns";
+import { LucideCalendar } from "lucide-react";
+import { ControllerRenderProps } from "react-hook-form";
+
+import { Calendar } from "./ui/calendar";
+import { FormControl } from "./ui/form";
+import { Button } from "./ui/button";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { PopoverClose } from "@radix-ui/react-popover";
+import { useRef } from "react";
+
+export function InputDate({ field }: { field: ControllerRenderProps }) {
+  const closeRef = useRef<null | HTMLButtonElement>(null);
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <FormControl>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "pl-3 font-normal w-full",
+              !field.value && "text-muted-foreground",
+            )}
+          >
+            {field.value ? (
+              formatDate(field.value, "PPP")
+            ) : (
+              <span>Pick a date</span>
+            )}
+            <LucideCalendar className="ml-auto h-4 w-4 opacity-50" />
+          </Button>
+        </FormControl>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="center">
+        <PopoverClose className="hidden" ref={closeRef}></PopoverClose>
+        <Calendar
+          mode="single"
+          selected={field.value || undefined}
+          onSelect={(...args) => {
+            field.onChange(...args);
+            closeRef.current?.click();
+          }}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
