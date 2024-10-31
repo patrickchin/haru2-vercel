e8388b43195f8ddf8604aa0bab042993db90f0d0
patrick <p.ch@live.co.uk>
2024-10-31 22:24:08 +0800

    popup on publish report

--- src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/publish-button.tsx
+++ src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/publish-button.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import * as Actions from "@/lib/actions";
-import { LucideBookOpenCheck } from "lucide-react";
+import { LucideBookOpenCheck, LucideLoader2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
 
 export function PublishButton({
   reportId,
@@ -11,17 +24,42 @@ export function PublishButton({
   reportId: number;
   disabled: boolean;
 }) {
+  const form = useForm();
+
   return (
-    <Button
-      variant="default"
-      disabled={disabled}
-      className="flex gap-2"
-      onClick={async () => {
-        await Actions.publishReport(reportId);
-      }}
-    >
-      Publish Report
-      <LucideBookOpenCheck className="h-4 w-4" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="default" disabled={disabled} className="flex gap-2">
+          Publish Report
+          <LucideBookOpenCheck className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Once published, the report cannot be edited.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(async () => {
+                await Actions.publishReport(reportId);
+              })}
+            >
+              <AlertDialogAction type="submit">
+                Yes, Publish Report
+                {form.formState.isSubmitting && (
+                  <LucideLoader2 className="animate-spin h-4" />
+                )}
+              </AlertDialogAction>
+            </form>
+          </Form>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
