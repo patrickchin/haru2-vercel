c3d4b88c35c0262bf81879133c984891130e4717
patrick <p.ch@live.co.uk>
2024-11-16 16:37:30 +0800

    feedback form improvements

--- src/components/feedback.tsx
+++ src/components/feedback.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as Actions from "@/lib/actions";
 
-import { LucideMessageSquare } from "lucide-react";
+import { LucideLoader2, LucideMessageSquare } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -16,6 +16,7 @@ import {
 } from "@/components/ui/dialog";
 import {
   Form,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -23,11 +24,12 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "./ui/textarea";
+import { useRef } from "react";
 
 function AddFeedbackForm() {
   const schema = z.object({
     email: z.string().optional(),
-    message: z.string(),
+    message: z.string().min(1, "Please enter a message!"),
   });
   type SchemaType = z.infer<typeof schema>;
 
@@ -35,21 +37,28 @@ function AddFeedbackForm() {
     resolver: zodResolver(schema),
   });
 
+  const closeRef = useRef<HTMLButtonElement>(null);
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(async (data: SchemaType) => {
           await Actions.addFeedback(data);
+          closeRef.current?.click();
         })}
         className="flex flex-col gap-4"
       >
         <FormField
           control={form.control}
           name="email"
-          render={({ field }) => (
-            <FormItem className="grow max-w-2xl">
-              <FormLabel>Email</FormLabel>
-              <Input {...field} />
+          render={({ field: { value, ...field2 } }) => (
+            <FormItem>
+              <FormLabel>Email (optional)</FormLabel>
+              <FormDescription>
+                Fill out your email for us to get back to you about your
+                feedback.
+              </FormDescription>
+              <Input {...field2} />
               <FormMessage />
             </FormItem>
           )}
@@ -58,19 +67,31 @@ function AddFeedbackForm() {
         <FormField
           control={form.control}
           name="message"
-          render={({ field }) => (
-            <FormItem className="grow max-w-2xl">
-              <FormLabel>Feedback Message</FormLabel>
-              <Textarea {...field} value={undefined} />
+          render={({ field: { value, ...field2 } }) => (
+            <FormItem>
+              <FormLabel>Your Message</FormLabel>
+              <FormDescription>
+                Tells us about any issues with the site or if you have specific
+                feature requests.
+              </FormDescription>
+              <Textarea {...field2} className="h-52" />
               <FormMessage />
             </FormItem>
           )}
         />
 
         <div className="flex gap-2 justify-end">
-          <DialogClose asChild>
-            <Button type="submit">Submit</Button>
-          </DialogClose>
+          <DialogClose className="hidden" ref={closeRef} />
+          <Button
+            type="submit"
+            disabled={!form.formState.isDirty || form.formState.isSubmitting}
+            className="flex items-center"
+          >
+            Submit
+            {form.formState.isSubmitting && (
+              <LucideLoader2 className="animate-spin w-3.5 h-3.5" />
+            )}
+          </Button>
         </div>
       </form>
     </Form>
