4f98fe42bedcec1631ca109de91f52d3075015ff
patrick <p.ch@live.co.uk>
2024-11-30 00:14:24 +0800

    lerger footer to fit feedback button

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full flex items-center justify-center bg-background border-t border-border">
-      <div className="flex-1 max-w-6xl py-4 px-8">
+      <div className="flex-1 max-w-6xl py-6 px-8">
         <p className="text-muted-foreground text-sm">© 2024 HarpaPro</p>
       </div>
     </footer>
