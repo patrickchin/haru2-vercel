0b4675bf325f7e5d17c0c8c9a85f5822d812cf78
patrick <p.ch@live.co.uk>
2024-12-06 05:40:59 +0800

    Add auto resize back to text area

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,18 +1,21 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { useAutoResizeTextarea } from "@/lib/hooks/use-auto-resize-textarea"
 
-const Textarea = React.forwardRef<
-  HTMLTextAreaElement,
-  React.ComponentProps<"textarea">
->(({ className, ...props }, ref) => {
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { autoResize?: boolean }
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, autoResize = false, ...props }, ref) => {
+  const { textAreaRef } = useAutoResizeTextarea(ref, autoResize)
   return (
     <textarea
       className={cn(
         "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
-      ref={ref}
+        ref={textAreaRef}
       {...props}
     />
   )
