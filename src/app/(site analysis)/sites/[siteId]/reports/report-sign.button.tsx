d83976e9fb4c290f2a65e907e1dad4a0ae412016
patrick <p.ch@live.co.uk>
2024-12-09 22:24:32 +0800

    Loading icon when signing report

--- src/app/(site analysis)/sites/[siteId]/reports/report-sign.button.tsx
+++ src/app/(site analysis)/sites/[siteId]/reports/report-sign.button.tsx
@@ -3,6 +3,8 @@
 import * as Actions from "@/lib/actions";
 import { SiteMemberRole } from "@/lib/types";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { LucideLoader2 } from "lucide-react";
 
 export function ReportSignButton({
   reportId,
@@ -15,12 +17,26 @@ export function ReportSignButton({
   buttonRole: SiteMemberRole;
   disabled?: boolean;
 }) {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <Button
       disabled={disabled || role !== buttonRole}
-      onClick={async () => Actions.signReport(reportId, buttonRole)}
+      onClick={async () => {
+        try {
+          setIsLoading(true);
+          await Actions.signReport(reportId, buttonRole);
+        } finally {
+          setIsLoading(false);
+        }
+      }}
     >
-      Click here to Sign
+      {isLoading ? (
+        <span className="flex gap-2 items-center">
+          Signing <LucideLoader2 className="animate-spin" />
+        </span>
+      ) : (
+        <span>Click here to Sign</span>
+      )}
     </Button>
   );
 }
