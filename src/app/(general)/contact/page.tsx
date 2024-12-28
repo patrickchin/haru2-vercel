c86c0bc3e94077d1b28040fc9f8eb668a6f8441f
patrick <p.ch@live.co.uk>
2024-12-28 08:17:27 +0800

    template contact and docs pages

---
+++ src/app/(general)/contact/page.tsx
@@ -0,0 +1,9 @@
+import { DefaultLayout } from "@/components/page-layouts";
+
+export default function Page() {
+  return (
+    <DefaultLayout>
+      Contact Us - Under Construction
+    </DefaultLayout>
+  );
+}
