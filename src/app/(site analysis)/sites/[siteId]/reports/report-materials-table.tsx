ef904b768f834c2ccadce7fe1f9f5addb83e98bc
patrick <p.ch@live.co.uk>
2025-01-22 01:18:28 +0800

    report materials make table filterable

---
+++ src/app/(site analysis)/sites/[siteId]/reports/report-materials-table.tsx
@@ -0,0 +1,165 @@
+"use client";
+
+import * as React from "react";
+import * as Actions from "@/lib/actions";
+import { SiteMaterial } from "@/lib/types";
+import useSWR from "swr";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export const columns: ColumnDef<SiteMaterial>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    size: 300,
+    cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "quantity",
+    header: () => <div className="text-right">Quantity</div>,
+    size: 100,
+    cell: ({ row }) => {
+      const quantity = row.getValue("quantity") as string;
+      const units = row.original.quantityUnit;
+      return (
+        <div className="text-right font-medium">
+          {quantity} {units}
+        </div>
+      );
+    } ,
+  },
+  {
+    accessorKey: "unitCost",
+    header: () => <div className="text-right">Unit Cost</div>,
+    size: 150,
+    cell: ({ row }) => {
+      const amount = row.original.unitCost ?? "-";
+      const currency = row.original.unitCostCurrency;
+      return (
+        <div className="text-right font-medium">
+          {amount} {currency}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "totalCost",
+    header: () => <div className="text-right">Total Cost</div>,
+    size: 150,
+    cell: ({ row }) => {
+      const amount = row.original.totalCost ?? "-"
+      const currency = row.original.unitCostCurrency;
+      return (
+        <div className="text-right font-medium">
+          {amount} {currency}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "condition",
+    header: () => <div className="text-left">Condition</div>,
+    size: 150,
+    cell: ({ row }) => {
+      return (
+        <div className="text-left font-medium">{row.getValue("condition")}</div>
+      );
+    },
+  },
+];
+
+export function MaterialsTable({ reportId }: { reportId?: number }) {
+  const { data: materials } = useSWR(`/api/report/${reportId}/materials`, () =>
+    reportId ? Actions.listSiteReportUsedMaterials(reportId) : undefined,
+  );
+
+  const table = useReactTable({
+    data: materials ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
+  return (
+    <div className="w-full pl-1">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter material names..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      style={{ width: `${header.getSize()}px` }}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
