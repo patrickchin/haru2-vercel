1a724916ca95816235c799232a094b618ec870e1
patrick <p.ch@live.co.uk>
2025-02-02 04:37:19 +0800

    offline-report-form make it simpler just js no ts

---
+++ offline-report-form/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Harpa Pro Construction Report Form
+      </header>
+    </div>
+  );
+}
