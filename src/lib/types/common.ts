f308125f787d294873eef11f7c85c86707d268cb
patrick <p.ch@live.co.uk>
2025-05-13 04:00:52 +0800

    omit stupid sid

--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -1,7 +1,7 @@
 import * as Schemas from "@/db/schema";
 
 export type AccountRole = (typeof Schemas.accountRoleEnum.enumValues)[number];
-export type HaruUserBasic = typeof Schemas.users1.$inferSelect;
+export type HaruUserBasic = Omit<typeof Schemas.users1.$inferSelect, "sid">;
 export type HaruUserDetailed = typeof Schemas.users1.$inferSelect & { email: string | null; };
 export type HaruUserAccount = typeof Schemas.accounts1.$inferSelect & HaruUserBasic;
 export type HaruFile = typeof Schemas.files1.$inferSelect & { uploader: HaruUserBasic | null; };
