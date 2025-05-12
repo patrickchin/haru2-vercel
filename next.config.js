9f0692a76140f0aae323dcef83e015c746c4bace
patrick <p.ch@live.co.uk>
2025-05-13 00:13:32 +0800

    remove blob store env var from next image config

--- next.config.js
+++ next.config.js
@@ -16,7 +16,6 @@ const nextConfig = {
       { hostname: `haru2.s3.me-central-1.amazonaws.com` },
       { hostname: `haru2-cape.s3.af-south-1.amazonaws.com` },
       { hostname: `${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com` },
-      { hostname: process.env.BLOB_STORE_HOSTNAME || "" },
     ],
   },
 };
