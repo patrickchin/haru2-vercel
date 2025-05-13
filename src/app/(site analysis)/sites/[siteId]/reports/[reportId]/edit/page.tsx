5ced258176d4011c4bfd014cbfdafa381ce109cc
patrick <p.ch@live.co.uk>
2025-05-13 22:26:53 +0800

    report header sticky shadow

--- src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/page.tsx
+++ src/app/(site analysis)/sites/[siteId]/reports/[reportId]/edit/page.tsx
@@ -58,7 +58,7 @@ export default async function Page({
 
   return (
     <DefaultLayout className="max-w-none relative pt-0">
-      <div className="w-full sticky top-0 z-30 py-4 bg-background border-b">
+      <div className="w-full sticky top-0 z-30 py-4 bg-background border-b shadow-md">
         <div className="max-w-5xl mx-auto">
           <EditReportHeader report={report} />
         </div>
